e21f3e1e08dc5937d55bce23b84a8aa10171a170
thomasquan605 <thomas.quan605@gmail.com>
2025-07-16 17:20:56 +0800

    页面搭建

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,23 +5,48 @@ export default defineConfig({
   description: "沉淀学习的地方",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
+      { 
+        text: '大前端', 
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'JavaScript', link: '/bigFE/JS/' },
+          { text: 'React', link: '/bigFE/React/' },
+        ] 
+      },
+      { text: '关于我', link: '/about' }
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+    sidebar: {
+      "/bigFE/": {
+        text: "大前端",
+        items: [
+          { text: '大前端', link: '/bigFE/' },
+          { text: 'JS', link: '/bigFE/JS/' },
+          { text: 'React', link: '/bigFE/React/' },
+        ]
+      },
+      "/bigFE/JS/": {
+        text: "html",
+        items: [
+          { text: 'JS1', link: '/bigFE/JS/js1' },
+          { text: 'JS2', link: '/bigFE/JS/js2' },
+        ]
+      },
+      "/bigFE/React/": {
+        text: "html",
+        items: [
+          { text: 'React', link: '/bigFE/React/' }
+        ]
+      },
+    
+    },
+
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ]
   }
 })
--- docs/index.md
+++ docs/index.md
@@ -4,22 +4,46 @@ layout: home
 
 hero:
   name: "银耳的博客"
-  text: "银耳沉淀学习的地方"
-  tagline: My great project tagline
+  text: "沉淀学习的地方"
+  tagline: 前端学习记录
+  image:
+    src: /logo.jpg
+    alt: logo
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 一起学习大前端
+      link: /bigFE
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 我的 github
+      link: https://github.com/rheazen
 
-features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+# features:
+#   - title: Feature A
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
 
+<style>
+:root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #282a29 10%, #70a288);
+
+  --vp-home-hero-image-background-image: linear-gradient(-45deg, #282a29 50%, #70a288 50%);
+  --vp-home-hero-image-filter: blur(44px);
+}
+
+@media (min-width: 640px) {
+  :root {
+    --vp-home-hero-image-filter: blur(56px);
+  }
+}
+
+@media (min-width: 960px) {
+  :root {
+    --vp-home-hero-image-filter: blur(68px);
+  }
+}
+</style>
